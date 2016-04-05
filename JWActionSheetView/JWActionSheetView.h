76d37ada5b4ae99906d06e19c03dcce1ca6379ee
AndersonLeeK <836654084@qq.com>
2016-04-05 17:20:21 +0800

    add readme

---
+++ JWActionSheetView/JWActionSheetView.h
@@ -0,0 +1,13 @@
+//
+//  JWActionSheetView.h
+//  JWActionSheetView
+//
+//  Created by Vinhome on 16/4/5.
+//  Copyright © 2016年 JW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JWActionSheetView : UIView
+
+@end
---
+++ JWActionSheetView/JWActionSheetView.m
@@ -0,0 +1,21 @@
+//
+//  JWActionSheetView.m
+//  JWActionSheetView
+//
+//  Created by Vinhome on 16/4/5.
+//  Copyright © 2016年 JW. All rights reserved.
+//
+
+#import "JWActionSheetView.h"
+
+@implementation JWActionSheetView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
